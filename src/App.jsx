a459db27c9f3573308aa556c521453be6b97e27d
Praveen Sharma <praying.sharma@gmail.com>
2023-09-01 11:54:51 +0530

    add files

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Run from "./Run";
+import Person from "./Person";
+import "./App.css";
+
+
+function App() {
+  return (
+    <>
+      <div>
+
+        <Person name="Praveen" age="23" />
+
+        
+     
+        
+
+        
+      </div>
+     
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/Person.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Person = ({name,age}) => {
+  return (
+    <div>
+
+       
+        <p>{name}</p>
+        <p>{age}</p>
+
+    </div>
+  )
+}
+
+export default Person
