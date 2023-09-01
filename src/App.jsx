df4843edab42e2e77948b8d671be75b1050dbfb3
Praveen Sharma <praying.sharma@gmail.com>
2023-09-01 12:25:48 +0530

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
-
 import Person from "./Person";
 import "./App.css";
 
 import Button from "./Button";
-
+import Header from "./Header";
+import List from "./List";
 
 function App() {
-  const msg=()=>{
-    alert("Hello")
-  }
+  const msg = () => {
+    alert("Hello");
+  };
+  const arr=[" one "," two "," three "]
   return (
     <>
       <div>
         <Person name="Praveen" age="23" />
-        
-       
 
         <Button text="This is Text" onClicke={msg} />
 
-        
-     
-        
+        <Header title="this is header tittle " />
 
         
+
+
+        {
+          arr.map((item,idx)=>{
+            return(
+              <List items={item}></List>     )
+              {/* <li key={idx}>{item}</li>  */}
+              })
+          
+        }
       </div>
-     
     </>
-    
   );
 }
 
